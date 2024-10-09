a3d46affabb5fc49cf1d31d313db83660fb3203b
SofyO <up210419@alumnos.upa.edu.mx>
2024-10-09 15:54:03 -0600

    Va jalando

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HOLAAA JOTO <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
